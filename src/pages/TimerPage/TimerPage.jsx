bf2588793a8aa3f012db12f60b8e6d3cea2eb79a
Sam Knight <robogamingbysam@icloud.com>
2022-01-11 12:48:14 -0600

    Fix broken import

--- src/pages/TimerPage/TimerPage.jsx
+++ src/pages/TimerPage/TimerPage.jsx
@@ -3,13 +3,15 @@ import { Card } from '../../stories/Card/Card';
 import { HeaderSubheader } from '../../stories/HeaderSubheader/HeaderSubheader';
 import { ProgressBar } from '../../stories/ProgressBar/ProgressBar';
 import { Sunset } from '../../stories/Sunset/Sunset';
-import { PropTypes } from 'react';
+import { PropTypes } from 'prop-types';
+import { UpcomingTasks } from '../../stories/UpcomingTasks/UpcomingTasks';
 
 export const TimerPage = ({ dark, ...props }) => {
   return (
     <Sunset>
       <Card timer={true} dark={dark}>
         <HeaderSubheader dark={dark}/>
+        <UpcomingTasks dark={dark}/>
         <ProgressBar />
       </Card>
     </Sunset>
--- src/stories/UpcomingTasks/UpcomingTasks.jsx
+++ src/stories/UpcomingTasks/UpcomingTasks.jsx
@@ -4,19 +4,15 @@ import PropTypes from 'prop-types';
 export const UpcomingTasks = ({ dark, ...props }) => {
   return (
     <div>
-      TODO: Implement UpcomingTasks
+      <p>TODO: Implement UpcomingTasks</p>
     </div>
   );
 };
 
 UpcomingTasks.propTypes = {
   dark: PropTypes.bool,
-  header: PropTypes.string,
-  subheader: PropTypes.string,
 };
 
 UpcomingTasks.defaultProps = {
   dark: true,
-  header: 'Pre-Calculus',
-  subheader: '25 minute sprint',
 };
